ab8275e82404f83182241fd9c8e038474e1d021c
NenadT <nenad.trajkov@gmail.com>
2021-05-11 11:38:23 +0200

    completed 8.15

---
+++ mooc-java-programming-ii/part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
@@ -0,0 +1,51 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Sakine
+ */
+public class StorageFacility {
+
+    private HashMap<String, ArrayList<String>> storage;
+
+    public StorageFacility() {
+        this.storage = new HashMap<>();
+    }
+
+    public void add(String unit, String item) {
+        this.storage.putIfAbsent(unit, new ArrayList<>());
+        this.storage.get(unit).add(item);
+    }
+
+    public ArrayList<String> contents(String storageUnit) {
+        return storage.getOrDefault(storageUnit, new ArrayList<>());
+    }
+
+    public void remove(String storageUnit, String item) {
+        if (!this.storage.containsKey(storageUnit)) {
+            return;
+        }
+        this.storage.get(storageUnit).remove(item);
+        if (this.storage.get(storageUnit).isEmpty()) {
+            this.storage.remove(storageUnit);
+        }
+    }
+
+    public ArrayList<String> storageUnits() {
+        ArrayList<String> listToReturn = new ArrayList<>();
+        for (String key : storage.keySet()) {
+            if (!storage.get(key).isEmpty()) {
+                listToReturn.add(key);
+            }
+        }
+        return listToReturn;
+    }
+
+}
