4e862d2cb728e3f0d56b5f36640300375563bc8e
NenadT <nenad.trajkov@gmail.com>
2021-05-11 12:31:27 +0200

    completed 9.1, 9.2

---
+++ mooc-java-programming-ii/part09-Part09_02.PersonAndSubclasses/src/main/java/Student.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sakine
+ */
+public class Student extends Person{
+    private int credits = 0;
+
+    public Student(String name, String address) {
+        super(name, address);
+    }
+    
+    public void study(){
+        credits += 1;
+    }
+
+    public int credits() {
+        return credits;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n  Study credits " + this.credits;
+    }
+    
+}
