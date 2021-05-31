f3072b6991ec814a580edcf3b6c8085a73c4def3
NenadT <nenad.trajkov@gmail.com>
2021-05-31 20:52:06 +0200

    Completed 11.5, 11.6

---
+++ mooc-java-programming-ii/part11-Part11_05.ThePlayerAndTheBot/src/main/java/Bot.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sakine
+ */
+public class Bot extends Player {
+
+    public Bot(String name) {
+        super(name);
+    }
+
+    @Override
+    public void play() {
+    }
+
+    public void addMove(String move) {
+
+    }
+}
